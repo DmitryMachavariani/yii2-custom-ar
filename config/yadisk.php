8db271089ab5d7ff42ad2c3acbe5e8550fbc8c9c
vladayson <vladaysin@gmail.com>
2019-03-09 15:38:02 +0300

    add local ya disk params

--- config/yadisk.php
+++ config/yadisk.php
@@ -1,8 +1,14 @@
 <?php
 
-return [
+$data = [
     'class' => \app\components\Storage::class,
     'token' => 'AQAAAAASG4GJAAWKs7d1FLgrhkE9p_J0VJfIxPo',
     'folder' => 'taskmgr',
     'localBaseFolder' => '@uploads'
 ];
+
+if (file_exists(dirname(__FILE__) . '/local.yadisk.php')) {
+    $data = \yii\helpers\ArrayHelper::merge($data, require (dirname(__FILE__) . '/local.yadisk.php'));
+}
+
+return $data;
